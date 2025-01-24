ac58de6f236ab2984ed34429d07c2ff7b4a88980
Tommyboy0797 <thomas.barley@btinternet.com>
2025-01-24 16:26:05 +0000

    Initial python server setup

---
+++ perf_calc.py
@@ -0,0 +1,4 @@
+import random
+
+def get_random_number():
+    random.randint(1,10)
--- server.py
+++ server.py
@@ -1,4 +1,27 @@
-print("hello")
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import perf_calc
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
-print("papiplanes was here")
+@app.get("/")
+def read_root(request: Request):
+    respo = templates.TemplateResponse(
+        "index.html", 
+        {"request": request,
+          "message": "Hello, FastAPI!",
+            "test": "Test test test",
+            "random_number": perf_calc.get_random_number()}
+        )
+        
+
+    return respo
+
+@app.get("home")
+def home_page(request: Request):
+
+
+
+
+    return
