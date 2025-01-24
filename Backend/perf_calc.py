000ded744e6782f1d74200be5ebeb85a1288f55c
Tommyboy0797 <thomas.barley@btinternet.com>
2025-01-24 17:20:49 +0000

    testing variables added to try and calc v1 @ 50% flaps

--- Backend/perf_calc.py
+++ Backend/perf_calc.py
@@ -1,4 +1,14 @@
 import random
 
+aircraft_grossweight = input("Enter aircraft gross weight. IE, 90 = 90,000lbs : ")
+pressure_altitude = input("enter outside pressure alt")
+outside_air_temp = input("Enter outside air temp")
+
+
+
 def get_random_number(): #testing stuff 
     random.randint(1,10)
+
+
+def calc_V1_50flaps(aircraft_grossweight):
+    print("asdf")
