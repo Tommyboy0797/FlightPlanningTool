e0f1cdfd89f55a3439978f3140dd863af70d9759
Tommyboy0797 <thomas.barley@btinternet.com>
2025-01-25 00:33:34 +0000

    init rotation speed code

---
+++ Backend/rotation_calc.py
@@ -0,0 +1,43 @@
+import random
+import os
+import re
+import numpy as np
+print(os.getcwd())
+from Backend.py_utils import *
+
+
+aircraft_grossweight = 1
+takeoff_factor = 5
+
+
+TOP_FOLDER = "Backend/chart_dig/completed-takeoff/amax-takeoff"
+DIG_FILE_NAME = "amax-speed-rotation.dig"
+
+data = {}
+
+chart = ParseDig(f'./{TOP_FOLDER}/dig/{DIG_FILE_NAME}')
+for c in  chart.curveNames():
+    yVector = [row [1] for row in chart.curve(c)]
+    xVector = [row [0] for row in chart.curve(c)]
+    scale_number = float(re.sub('[^0-9]','', c.replace("-", "_")))
+    
+    data[scale_number] = {
+        "x": xVector,
+        "y": yVector
+    }
+
+def get_rotation_speed(gross_wt, takeoff_factor, data):
+        
+    x_values = []
+    y_values = []
+    
+    for this_gross_weight, this_scales_data in data.items():
+        
+        x_values.append(this_gross_weight)
+        y_values.append(round(np.interp(takeoff_factor, this_scales_data["x"], this_scales_data["y"]), 2))       
+        print("Takeoff factor: ", takeoff_factor)
+    return round(np.interp(gross_wt, x_values, y_values), 2)
+    
+
+
+    
