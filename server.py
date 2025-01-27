e9d18c191138752abdd52c4370654d128e24f058
Tommyboy0797 <thomas.barley@btinternet.com>
2025-01-27 19:55:38 +0000

    fix api request

--- server.py
+++ server.py
@@ -15,40 +15,30 @@ def read_root(request: Request):
         "index.html", 
         {"request": request,
           "title": "TOLD TEST",
-            "uncorrected_max_eff_TO_dist_text": "Uncorrected maximum effort takeoff distance: ",
-            "uncorrected_max_eff_TO_dist": perf_calc.try_get_uncorrected_max_eff_field_length(perf_calc.aircraft_grossweight, perf_calc.takeoff_factor, perf_calc.data),
-            "gross_weight_text": perf_calc.aircraft_grossweight,
-            "takeoff_factor_text": perf_calc.takeoff_factor,
-            "rotation_speed_calculated": rotation_calc.get_rotation_speed(perf_calc.aircraft_grossweight, perf_calc.takeoff_factor, rotation_calc.data),
-            "runway_avail": perf_calc.rwy_available,
-            "uncorrected_refusal_test": refusal.get_refusal_p1(perf_calc.takeoff_factor, perf_calc.rwy_available, refusal.data),
-            "uncorrected_refusal_test_p2": refusal.get_refusal_p2(refusal.get_refusal_p1(perf_calc.takeoff_factor, perf_calc.rwy_available, refusal.data), perf_calc.aircraft_grossweight, refusal.data1)
-
             }
-    )
+
         
-    
+    )
     return respo
 
 
 # endpoint to recieve value for gross weight
 @app.get("/get_gwt") # mailbox (what we are listening on), get is request type -> serving get 
-def handle_gwt(gwt):
+def handle_gwt(request: Request,gwt,get_to_factor,get_rwy_available):
     gwt = float(gwt)
     perf_calc.aircraft_grossweight = gwt
     print(gwt)
     respo = templates.TemplateResponse(
         "index.html", 
         {"request": request,
-          "title": "TOLD TEST",
             "uncorrected_max_eff_TO_dist_text": "Uncorrected maximum effort takeoff distance: ",
-            "uncorrected_max_eff_TO_dist": perf_calc.try_get_uncorrected_max_eff_field_length(perf_calc.aircraft_grossweight, perf_calc.takeoff_factor, perf_calc.data),
-            "gross_weight_text": perf_calc.aircraft_grossweight,
-            "takeoff_factor_text": perf_calc.takeoff_factor,
-            "rotation_speed_calculated": rotation_calc.get_rotation_speed(perf_calc.aircraft_grossweight, perf_calc.takeoff_factor, rotation_calc.data),
-            "runway_avail": perf_calc.rwy_available,
-            "uncorrected_refusal_test": refusal.get_refusal_p1(perf_calc.takeoff_factor, perf_calc.rwy_available, refusal.data),
-            "uncorrected_refusal_test_p2": refusal.get_refusal_p2(refusal.get_refusal_p1(perf_calc.takeoff_factor, perf_calc.rwy_available, refusal.data), perf_calc.aircraft_grossweight, refusal.data1)
+            "uncorrected_max_eff_TO_dist": perf_calc.try_get_uncorrected_max_eff_field_length(gwt, get_to_factor, perf_calc.data),
+            "gross_weight_text": gwt,
+            "takeoff_factor_text": get_to_factor,
+            "rotation_speed_calculated": rotation_calc.get_rotation_speed(gwt, get_to_factor, rotation_calc.data),
+            "runway_avail": get_rwy_available,
+            "uncorrected_refusal_test": refusal.get_refusal_p1(get_to_factor, get_rwy_available),
+            "uncorrected_refusal_test_p2": refusal.get_refusal_p2(refusal.get_refusal_p1(get_to_factor, get_rwy_available), gwt)
 
             }
     )
