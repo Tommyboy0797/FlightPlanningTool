0f992c79475e552492603e5bfeb5d8dbb42d22c8
Tommyboy0797 <thomas.barley@btinternet.com>
2025-02-10 23:11:14 +0000

    cleanup

--- server.py
+++ server.py
@@ -42,8 +42,7 @@ def handle_data(request: Request,gwt,get_to_factor,get_rwy_available, get_rwy_sl
         "uncorrected_refusal_test_p2": refusal.get_refusal_p2(refusal.get_refusal_p1(get_to_factor, get_rwy_available), gwt),
         "partially_corrected_refusal_p3": refusal.get_refusal_p3(refusal.get_refusal_p2(refusal.get_refusal_p1(get_to_factor, get_rwy_available), gwt), get_rwy_slope),
         "runway_slope": refusal.rwy_slope,
-        "sids": database_handler.get_sids(handle_route.origin_airfield)
-        # "user_route": handle_route.route,
+
     }
        
     return respo
