0a6c1b56aa869f13d4b43d00acd5ab864fe94fcf
Tommyboy0797 <thomas.barley@btinternet.com>
2025-02-10 22:45:41 +0000

    update to work without refreshing as much

--- server.py
+++ server.py
@@ -29,30 +29,24 @@ def read_root(request: Request):
 # endpoint to recieve value for gross weight
 @app.get("/get_data") # mailbox (what we are listening on), get is request type -> serving get 
 def handle_data(request: Request,gwt,get_to_factor,get_rwy_available, get_rwy_slope, enter_db_country):
-
     gwt = float(gwt)
     perf_calc.aircraft_grossweight = gwt
-
-    respo = templates.TemplateResponse(
-        "index.html", 
-        {"request": request,
-            "uncorrected_max_eff_TO_dist_text": "Uncorrected maximum effort takeoff distance: ",
-            "uncorrected_max_eff_TO_dist": perf_calc.try_get_uncorrected_max_eff_field_length(gwt, get_to_factor, perf_calc.data),
-            "gross_weight_text": round(gwt * 1000),
-            "takeoff_factor_text": get_to_factor,
-            "rotation_speed_calculated": rotation_calc.get_rotation_speed(gwt, get_to_factor, rotation_calc.data),
-            "runway_avail": get_rwy_available,
-            "uncorrected_refusal_test": refusal.get_refusal_p1(get_to_factor, get_rwy_available),
-            "uncorrected_refusal_test_p2": refusal.get_refusal_p2(refusal.get_refusal_p1(get_to_factor, get_rwy_available), gwt),
-            "partially_corrected_refusal_p3": refusal.get_refusal_p3(refusal.get_refusal_p2(refusal.get_refusal_p1(get_to_factor, get_rwy_available), gwt), get_rwy_slope),
-            "runway_slope": refusal.rwy_slope,
-            "sids": database_handler.get_sids(handle_route.origin_airfield)
-            # "user_route": handle_route.route,
-            }
-    )
-        
+    respo = {
+        "uncorrected_max_eff_TO_dist_text": "Uncorrected maximum effort takeoff distance: ",
+        "uncorrected_max_eff_TO_dist": perf_calc.try_get_uncorrected_max_eff_field_length(gwt, get_to_factor, perf_calc.data),
+        "gross_weight_text": round(gwt * 1000),
+        "takeoff_factor_text": get_to_factor,
+        "rotation_speed_calculated": rotation_calc.get_rotation_speed(gwt, get_to_factor, rotation_calc.data),
+        "runway_avail": get_rwy_available,
+        "uncorrected_refusal_test": refusal.get_refusal_p1(get_to_factor, get_rwy_available),
+        "uncorrected_refusal_test_p2": refusal.get_refusal_p2(refusal.get_refusal_p1(get_to_factor, get_rwy_available), gwt),
+        "partially_corrected_refusal_p3": refusal.get_refusal_p3(refusal.get_refusal_p2(refusal.get_refusal_p1(get_to_factor, get_rwy_available), gwt), get_rwy_slope),
+        "runway_slope": refusal.rwy_slope,
+        "sids": database_handler.get_sids(handle_route.origin_airfield)
+        # "user_route": handle_route.route,
+    }
+       
     return respo
-
 
 # endpoint for airports
 @app.get("/fetch_airports")
