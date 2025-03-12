319832f7bfe3f05403b9e825ce2c610bb5eeb57a
mcdelaney <mcdelaney@gmail.com>
2025-03-12 12:16:37 -0600

    remove scripts that should not be used

--- scripts/build_and_push_dev.ps1
+++
@@ -1,3 +0,0 @@
-
-podman build -t flightplanning:dev . ;
-podman push flightplanning:dev docker://gcr.io/dcs-analytics-257714/flightplanning:dev;
--- scripts/build_and_run_local.ps1
+++
@@ -1,3 +0,0 @@
-
-podman build -t flightplanning:local . ;
-podman run -p 8000:8000 flightplanning:local;
