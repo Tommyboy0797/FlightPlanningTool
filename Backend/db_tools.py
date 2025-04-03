16540ba62db03b0261cdf2d5d3766e7d4ce39f6f
mcdelaney <mcdelaney@gmail.com>
2025-04-03 16:45:45 -0600

    split database stuff into file make code better

---
+++ Backend/db_tools.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+import logging
+import sqlite3
+from jose import jwt
+from passlib.context import CryptContext
+## ----------------------------------------------- ## AUTHENTICATION ## ----------------------------------------------- ##
+
+# Secret key for JWT
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Initialize database
+def init_db():
+    logging.info('Initializing database')
+    conn = sqlite3.connect("database/users.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,
+            hashed_password TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
+    logging.info('Database setup complete')
+
+def decode_token(token: str) ->str:
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+# Password Hashing
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+# Token Generation
+def create_access_token(username: str):
+    expire = datetime.utcnow() + timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+    return jwt.encode({"sub": username, "exp": expire}, SECRET_KEY, algorithm=ALGORITHM)
