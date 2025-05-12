72a9132f1cc2b3fb492a96cc2c35cf5f6c8d7443
Tommyboy0797 <thomas.barley@btinternet.com>
2025-05-12 21:04:34 +0100

    refactor to not use anything globally

--- Backend/rotation_calc.py
+++ Backend/rotation_calc.py
@@ -6,33 +6,37 @@ print(os.getcwd())
 from Backend.py_utils import *
 from Backend import perf_calc as perf_calc
 
-TOP_FOLDER = "Backend/chart_dig/completed-takeoff/amax-takeoff"
-DIG_FILE_NAME = "amax-speed-rotation.dig"
+def get_rotation_speed(gross_wt, takeoff_factor):
+    TOP_FOLDER = "Backend/chart_dig/completed-takeoff/amax-takeoff"
+    DIG_FILE_NAME = "amax-speed-rotation.dig"
 
-data = {}
+    # Parse the dig file
+    data = {}
+    chart = ParseDig(f'./{TOP_FOLDER}/dig/{DIG_FILE_NAME}')
+    for c in chart.curveNames():
+        yVector = [row[1] for row in chart.curve(c)]
+        xVector = [row[0] for row in chart.curve(c)]
+        scale_number = float(re.sub('[^0-9]', '', c.replace("-", "_")))
 
-chart = ParseDig(f'./{TOP_FOLDER}/dig/{DIG_FILE_NAME}')
-for c in  chart.curveNames():
-    yVector = [row [1] for row in chart.curve(c)]
-    xVector = [row [0] for row in chart.curve(c)]
-    scale_number = float(re.sub('[^0-9]','', c.replace("-", "_")))
-    
-    data[scale_number] = {
-        "x": xVector,
-        "y": yVector
-    }
+        data[scale_number] = {
+            "x": xVector,
+            "y": yVector
+        }
 
+    # Prepare the interpolation arrays
+    weight_scale_set = []  # Gross weight scale (e.g., 30000, 40000, 50000)
+    results_based_on_to_factor = []  # Rotation speed for each gross weight based on takeoff factor
 
-def get_rotation_speed(gross_wt, takeoff_factor, data):
-        
-    x_values = []
-    y_values = []
-    
-    for this_gross_weight, this_scales_data in data.items():
-        x_values.append(this_gross_weight)
-        y_values.append(round(np.interp(takeoff_factor, this_scales_data["x"], this_scales_data["y"]), 2))     
-    return round(np.interp(gross_wt, x_values, y_values))
-    
+    for weight_scale, xy_pairs in data.items():
+        x_values = xy_pairs["x"]
+        y_values = xy_pairs["y"]
 
+        weight_scale_set.append(weight_scale)
+        results_based_on_to_factor.append(round(np.interp(takeoff_factor, x_values, y_values), 2))
+
+    # Interpolate for the given gross weight
+    rotation_speed = np.interp(gross_wt, weight_scale_set, results_based_on_to_factor)
+
+    return round(rotation_speed)
 
     
