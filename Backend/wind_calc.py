18d7247cac7effd138ec015717e09cc5c507b39c
Tommyboy0797 <thomas.barley@btinternet.com>
2025-05-12 21:04:11 +0100

    fix dependency on runway being selected

--- Backend/wind_calc.py
+++ Backend/wind_calc.py
@@ -7,6 +7,9 @@ def calc_winds(runwayhdg,windhdg):
     if windhdg == None: # variable wind direction
         windhdg = "0"
 
+    if not runwayhdg or not runwayhdg[0]:
+        runwayhdg = [[0]] # default north if there is not a selected runway yet
+        
     runwayhdg = int(runwayhdg[0][0]) # runwayhdg is a list, we get out a tuple with the first [0], then the value with the second
 
     windhdg = int(windhdg)
